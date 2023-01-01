31bc1881541a7c4e1d1bc7dd9b03e82fa3c3d038
Miranda <120993025+Miranda-Bai@users.noreply.github.com>
2023-01-01 13:59:47 +1300

    todolist

---
+++ ToDoList/src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import TDLHeader from './components/TDLHeader.vue'
+import TDLFooter from './components/TDLFooter.vue'
+import TDLList from './components/TDLList.vue'
+import TDLItem from './components/TDLItem.vue'
+
+export default {
+    name:'App',
+    components: {
+    TDLFooter,
+    TDLHeader,
+    TDLList,
+    TDLItem,
+  }
+}
+
+</script>
+
+<template>
+<TDLHeader></TDLHeader>
+<TDLList>
+  <TDLItem></TDLItem>
+</TDLList>
+<TDLFooter></TDLFooter>
+</template>
+<style >
+
+</style>
---
+++ ToDoList/src/components/TDLHeader.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+<script>
+export default {
+    name:'TDLHeader',
+}
+</script>
+<style scoped>
+    
+</style>
